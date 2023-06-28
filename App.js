328c125a0443639e7ea986c0c9b213cdd80f5da1
Mr. Shah <120000782+Shahupdates@users.noreply.github.com>
2023-06-28 14:26:20 -0400

    Create App.js

---
+++ App.js
@@ -0,0 +1,48 @@
+// App.js
+
+import React, { Component } from 'react';
+import Web3 from 'web3';
+import './App.css';
+import VotingContract from './contracts/Voting.json';
+
+class App extends Component {
+
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+
+    const networkId = await web3.eth.net.getId()
+    const networkData = VotingContract.networks[networkId]
+    if(networkData) {
+      const voting = new web3.eth.Contract(VotingContract.abi, networkData.address)
+      this.setState({ voting })
+      const candidatesCount = await voting.methods.candidatesCount().call()
+      this.setState({ candidatesCount })
+
+      for (let i = 1; i <= candidatesCount; i++) {
+        const candidate = await voting.methods.candidates(i).call()
+        this.setState({ 
+          candidates: [...this.state.candidates, candidate] 
+        })
+      }
+    } else {
+      window.alert('Voting
